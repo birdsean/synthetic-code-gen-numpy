82265628adabddebb070e7d282327a50693ec73d
Sean Bird <bird.seanmichael@gmail.com>
2023-09-21 21:28:52 -0600

    Refactor function for generating invocation outputs

--- main.py
+++ main.py
@@ -65,9 +65,10 @@ def get_potential_inputs(function_code):
                 + function_code
                 + "\n```\nReturn a list of valid inputs that would thoroughly test "
                 + "the capabilities of the function. Your answer should be a list of ten new line separated list of cases, where each "
-                + "case is a comma separated list of function parameter values. For example, if the function was slice_string(str, start_index, "
+                + "case is a comma separated list of function parameter values.\nFor example, if the function was slice_string(str, start_index, "
                 + "end_index), your answer would look something like:\n\nANSWER:\n'foobar',0,3\n'test_string_2,0,0'\nJust respond with inputs, NO "
-                + "COMMENTARY, NO NUMBERING, NO LABELS, NO BULLET POINTS, NO DASHES. If the param value is a string, be sure to surround it with double quotes.",
+                + "COMMENTARY, NO NUMBERING, NO LABELS, NO BULLET POINTS, NO DASHES. If the param value is a string, be sure to surround it with double quotes."
+                + "If the function takes no params, return an empty line. Each line should be unique."
             },
         ],
     )
@@ -113,9 +114,9 @@ def execute_inputs(function_name, invocations):
 
         # Check for errors
         if process.returncode == 0:
-            results[invocation] = ("stdout", stdout.decode("utf-8"))
+            results[invocation] = ["stdout", stdout.decode("utf-8")]
         else:
-            results[invocation] = ("stderr", stderr.decode("utf-8"))
+            results[invocation] = ["stderr", stderr.decode("utf-8")]
     return results
 
 
@@ -144,10 +145,19 @@ def generate_invocation_outputs():
         if function_name not in public_functions:
             del functions[function_name]
 
+    # filter out functions that are already in master_invocations.json
+    if os.path.exists("master_invocations.json"):
+        with open("master_invocations.json", "r") as f:
+            master_invocations = json.load(f)
+
+        for function_name in list(functions.keys()):
+            if function_name in master_invocations:
+                del functions[function_name]
+
     print(f"Generating invocation outputs for {len(functions)} functions")
     for i, function_name in enumerate(functions.keys()):
         print(
-            f"Generating invocation outputs for {function_name}. {i+1}/{len(functions)} done."
+            f"Generating invocation outputs for {function_name}. {i+1}/{len(functions)}."
         )
         if function_name not in public_functions:
             continue
@@ -168,7 +178,12 @@ def generate_invocation_outputs():
         cache["outputs"] = outputs
 
         master_invocations[function_name] = cache
-        break
+
+        with open("master_invocations.json", "w") as f:
+            json.dump(master_invocations, f)
+        
+        if i > 15:
+            break
 
     print(f"Generated {len(master_invocations)} invocation outputs")
     with open("master_invocations.json", "w") as f:
